ead5c621ac867b5e043e98f6409deff51879c97e
AJIT <ajit.palakkad@gmail.com>
2015-09-14 20:17:04 +0530

    Built webapplied from commit 87a5f58 on branch master

--- server.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ajit on 13-09-2015.
- */
-var express = require('express');
-var app = express();
-app.use(express.static(__dirname));
-var port = process.env.PORT||3000;
-app.listen(port);
-console.log('list'+port);
